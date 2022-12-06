9ddc908a2898d2d9220bc5a5eefe29b36b77e1a6
陈葳蕤 <chenweirui@chenweiruideMacBook-Pro-2.local>
2022-12-06 21:05:47 +0800

    update info

--- cloudfunctions/inithandler/userInfo/index.js
+++ cloudfunctions/inithandler/userInfo/index.js
@@ -6,49 +6,31 @@ cloud.init({
 const db = cloud.database();
 const userUserCollection = db.collection('user-info');
 const userCardCollection = db.collection('user-card-one');
+
+// 获取一班班级信息
+exports.main = async (event, context) => {
+  let result;
+  switch (event.params.key) {
+      case 'add': result = await addRecord(event.params.body); break;
+      case 'get': result = await getUserRecord(); break;
+      // 信息发布到班级
+      case 'publicToClass': result = await publicMySelf(); break;
+      // 同步信息
+      case 'syncInfo' : result = await syncInfo(event.params.body); break;
+  }
+  return {
+      data: result
+  };
+};
+
 // 添加用户信息
 async function addRecord(body) {
-  console.log('body:' + JSON.stringify(body));
   // 获取基础信息
   const wxContext = cloud.getWXContext();
   // 先查询是否班级卡片有此同学 并更新相关信息到卡片展现
-  let haveShowCard;
-  if(body.wxcode){
-    haveShowCard = await userCardCollection.where({
-      wxcode: body.wxcode
-    }).get();
-    // 更新除_id类信息
-    if(haveShowCard.data && haveShowCard.data.length > 0){
-      let {...originCardBody} = haveShowCard.data[haveShowCard.data.length-1];
-      let finalBody = {
-        openid: wxContext.OPENID,
-        ...originCardBody,
-        ...body
-      }
-      let {_id, ...deleteBody} = finalBody;
-      console.log('deleteBodydeleteBody:' + JSON.stringify(deleteBody));
-      // 如果存在openid 则用openid来修改
-      if(originCardBody.openid && originCardBody.openid !== ''){
-        userCardCollection.where({
-          wxcode: body.wxcode
-        }).update({
-          // data 字段表示需新增的 JSON 数据
-          data: deleteBody
-        }).catch(e =>{
-          console.log('------------------------:' + e);
-        });
-      } else {
-        userCardCollection.where({
-          wxcode: body.wxcode
-        }).update({
-          // data 字段表示需新增的 JSON 数据
-          data: deleteBody
-        }).catch(e =>{
-          console.log('------------------------:' + e);
-        });
-      }
-    }
-  }
+  let haveShowCard = await userCardCollection.where({
+    openid: wxContext.OPENID
+  }).get();
   // 先查询是否存在用户 存在则更新用户信息
   let isLive =  await userUserCollection.where({
     openid: wxContext.OPENID
@@ -110,15 +92,105 @@ async function getUserRecord() {
       return selfData
     }
   }
-
-// 获取一班班级信息
-exports.main = async (event, context) => {
-    let result;
-    switch (event.params.key) {
-        case 'add': result = await addRecord(event.params.body); break;
-        case 'get': result = await getUserRecord(); break;
+// 发布信息到班级
+async function publicMySelf() {
+  // 获取openId基础信息
+  const wxContext = cloud.getWXContext();
+  // 获取个个人信息
+  let cardInfo = await userCardCollection.where({
+    openid: wxContext.OPENID
+  }).get();
+  let ownerInfo = await userUserCollection.where({
+    openid: wxContext.OPENID
+  }).get();
+  let ownerbody = {}
+  if(ownerInfo.data.length > 0){
+    ownerbody = ownerInfo.data[ownerInfo.data.length -1];
+  }
+  // 更新除_id类信息
+  if(cardInfo.data && cardInfo.data.length > 0){
+    let {...originCardBody} = cardInfo.data[cardInfo.data.length-1];
+    let finalBody = {
+      openid: wxContext.OPENID,
+      ...originCardBody,
+      ...ownerbody
     }
-    return {
-        data: result
-    };
-};
+    let {_id, ...deleteBody} = finalBody;
+    // 如果存在openid 则用openid来修改
+     return await userCardCollection.where({
+        openid: originCardBody.openid
+      }).update({
+        data: deleteBody
+      }).catch(e =>{
+      });
+    } else {
+      return await userCardCollection.add({
+        data: ownerbody
+      })
+    }
+}
+// 同步信息
+async function syncInfo(body) {
+  // 获取基础信息
+  const wxContext = cloud.getWXContext();
+  if(!body.wxcode) {
+    return {};
+  } else {
+    let haveShowCard = await userCardCollection.where({
+      wxcode: body.wxcode
+    }).get();
+    console.log('haveShowCard:' + JSON.stringify(haveShowCard));
+    // 更新除_id类信息
+    if(haveShowCard.data && haveShowCard.data.length > 0){
+      let {...originCardBody} = haveShowCard.data[haveShowCard.data.length-1];
+      let finalBody = {
+        openid: wxContext.OPENID,
+        ...body,
+        ...originCardBody,
+      }
+      let {_id, ...deleteBody} = finalBody;
+      // 如果存在openid 则用openid来修改
+      if(originCardBody.openid && originCardBody.openid !== ''){
+        if(wxContext.OPENID !== originCardBody.openid) {
+          return;
+        }
+       await userCardCollection.where({
+          openid: wxContext.OPENID
+        }).update({
+          // data 字段表示需新增的 JSON 数据
+          data: deleteBody
+        }).catch(e =>{
+        });
+        // 更新个人信息
+       await userUserCollection.where({
+          openid: wxContext.OPENID,
+        }).update({
+          data: deleteBody
+        }).catch(e =>{
+        });
+        return deleteBody;
+      } else {
+        console.log('deleteBody:' + JSON.stringify(deleteBody));
+        console.log('wxContext.OPENID:' + wxContext.OPENID);
+        // 更新卡片
+        await userCardCollection.where({
+          wxcode: body.wxcode
+        }).update({
+          // data 字段表示需新增的 JSON 数据
+          data: deleteBody
+        }).catch(e =>{
+        });
+        // 更新个人信息
+        await userUserCollection.where({
+          openid: wxContext.OPENID,
+        }).update({
+          data: deleteBody
+        }).catch(e =>{
+        });
+        return deleteBody;
+      }
+    }
+  }
+}
+
+
