8104fbb07fbed566bae829c63a23fd990960f2a1
GeeMing <geeming@foxmail.com>
2023-04-08 11:22:38 +0800

    已认证状态; 更新样式

--- miniprogram/pages/infoAuth/infoAuth.js
+++ miniprogram/pages/infoAuth/infoAuth.js
@@ -8,16 +8,18 @@ Page({
       title: "实名认证",
       desc: "连接公安系统大数据验证身份真实性",
       path: '/pages/infoAuth/idCard/idCard',
+      authStep: 0,// 0，1，2 => 未认证，审核中，已认证
     }, {
       title: "工作认证",
       desc: "互联网/金融/事业单位等优质青年聚集地",
       path: '/pages/infoAuth/job/job',
+      authStep: 0,
     }, {
       title: "学历认证",
       desc: "985/211/名校/海归等高学历人群聚集地",
       path: '/pages/infoAuth/education/education',
-    }],
-    studentInfo: {}
+      authStep: 0,
+    }]
   },
 
   async onShow() {
@@ -26,7 +28,27 @@ Page({
     console.log("GET student info", res);
     let studentInfo = res.data
     app.globalData.studentInfo = studentInfo
-    this.setData({ studentInfo })
+    if (studentInfo.has) {
+
+      if (studentInfo.info.idCardValid) {
+        this.data.menu[0].authStep = 2
+      }
+      if (studentInfo.info.educationImages) {
+        this.data.menu[1].authStep = 1
+      }
+      if (studentInfo.info.jobAuthValid) {
+        this.data.menu[1].authStep = 2
+      }
+
+      if (studentInfo.info.jobImages) {
+        this.data.menu[2].authStep = 1
+      }
+      if (studentInfo.info.educationAuthValid) {
+        this.data.menu[2].authStep = 2
+      }
+      this.setData({ menu: this.data.menu })
+    }
+
   },
   onAuthClick(e) {
     let index = e.currentTarget.dataset.index
