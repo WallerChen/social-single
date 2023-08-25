1c5b6974e356a7426f81f293a44a32f0f3fa68f9
GeeMing <geeming@foxmail.com>
2023-08-25 13:41:55 +0800

    add: 照片墙

--- miniprogram/pages/info/components/detail/index.js
+++ miniprogram/pages/info/components/detail/index.js
@@ -2,15 +2,15 @@ Component({
   properties: {
     desc: String,
     imageList: Array,
-    avatarUrl: String,
+    avatarUrl: String
   },
   methods: {
     previewImage(e) {
-      const { image } = e.currentTarget.dataset;
-      wx.previewImage({ urls: [image], current: image });
+      const { image } = e.currentTarget.dataset
+      wx.previewImage({ urls: [image], current: image })
     },
     close() {
-      this.triggerEvent('close');
-    },
-  },
-});
+      this.triggerEvent('close')
+    }
+  }
+})
